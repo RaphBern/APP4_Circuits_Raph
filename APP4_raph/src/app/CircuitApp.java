07861eec00b1413956986585505768e2f3eb9bf9
raph <2541482@cegeplimoilou.ca>
2026-04-04 16:11:08 -0400

    Début circuipApp

--- APP4_raph/src/app/CircuitApp.java
+++ APP4_raph/src/app/CircuitApp.java
@@ -2,11 +2,23 @@ package app;
 
 import electronique.Composant;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CircuitApp {
    public CircuitApp() {
-       CircuitBuiler builer = new CircuitBuiler();
-       Composant c = builer.construireCircuit(System.getProperty("user.dir") + "\\APP4_raph\\src\\donnees\\fichiers_json\\complexe_industriel_zone_nord.json");
-       System.out.println(c.calculerResistance());
+//       CircuitBuiler builer = new CircuitBuiler();
+//       Composant c = builer.construireCircuit(System.getProperty("user.dir") + "\\APP4_raph\\src\\donnees\\fichiers_json\\reseau_secours_hopital.json");
+//       System.out.println(c.calculerResistance());
+
+       Scanner s = new Scanner(System.in);
+       if (s.nextLine().equals("R")) {
+          new CircuitApp();
+       } else if (s.nextLine().equals("Q")) {
+           System.out.println("Au revoir");
+           s.close();
+       }
 
    }
 
